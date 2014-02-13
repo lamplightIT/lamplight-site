6fc587ea218d613dba8fcbf68e2c1f85fd4e0bd9
fernando <fernando@fernando-G31M-ES2C.(none)>
2014-02-13 13:47:34 -0200

    menu scrolling is set

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+/*
+	Menu function that scroll to the correct page.
+*/
+function scrollToPage(id) {
 
+	var to = ('#'+id);
+
+	$("body").animate({
+		scrollTop: $(to).offset().top
+	});
+
+}
+
+$(document).ready(function(){
+
+	/*
+		Trigger the function when a menu link is clicked.
+	 */
+	$('.menu a').click(function(){
+
+		var id = $(this).attr('to');
+		scrollToPage(id);
+
+	});
+
+});
